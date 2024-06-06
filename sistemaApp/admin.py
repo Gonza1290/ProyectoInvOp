95cd0b4a3e3d1eef7ce36a64bffe7c01e865a928
Gonzalo Cayo <gonzacayo@gmail.com>
2024-06-06 16:24:43 -0300

    Modulo maestro articulo, ABM OC, ABM Proveedor

--- sistemaApp/admin.py
+++ sistemaApp/admin.py
@@ -1,3 +1,97 @@
 from django.contrib import admin
+from .models import Articulo,FamiliaArticulo,ModeloInventario,EstadoOrdenCompra,OrdenCompra,Proveedor
+from django.db.models import F  # Agregar la importación de F
+from django.db.models import Exists, OuterRef
 
-# Register your models here.
+class FamiliaArticuloAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nombre_familia', 'modeloInventario', 'fechaHoraBajaFA')
+    search_fields = ('id','nombreFamiliaArticulo')
+    ordering = ('id',)
+    list_display_links = ('nombre_familia',)
+    
+    
+    #Metodo para mostrar otro nombre distinto del atributo
+    def nombre_familia(self, obj):
+        return obj.nombreFamiliaArticulo
+    nombre_familia.short_description = 'Nombre de la Familia'
+    
+    # Action para dar de baja logica a FA
+    def Eliminacion_Logica(modeladmin, request, queryset):
+        for articulo in queryset:
+            articulo.dar_de_baja()
+    Eliminacion_Logica.short_description = "Eliminacion Logica"
+    
+    # Action para dar de baja logica a FA
+    def Activacion_Logica(modeladmin, request, queryset):
+        for articulo in queryset:
+            articulo.dar_de_alta()
+    Activacion_Logica.short_description = "Activacion Logica"
+    
+    #Agrego accion
+    actions = [Eliminacion_Logica,Activacion_Logica]
+    
+#Clase que define un filtro personalizado en articulos
+class ArticuloFaltanteFilter(admin.SimpleListFilter):
+    title = 'Estado de Stock'  # Título del filtro que aparecerá en la interfaz de administración
+    parameter_name = 'estado_stock'  # Nombre del parámetro de URL para este filtro
+
+    def lookups(self, request, model_admin):
+        # Define las opciones del filtro
+        return (
+            ('faltantes', 'Artículos faltantes'),  # Opción para artículos con stockActual < stockSeguridad
+            ('reponer', 'Articulos a Reponer'),  # Opción para artículos con stockActual < puntoPedido
+        )
+
+    #queryset inicialmente tienen todos los articulos existentes
+    def queryset(self, request, queryset):
+        if self.value() == 'faltantes':
+            return queryset.filter(stockActual__lt=F('stockSeguridad'))
+        elif self.value() == 'reponer':
+            subquery = OrdenCompra.objects.filter(  #subquery es una subconsulta
+                articulo_id=OuterRef('pk'),
+                estadoOrdenCompra__nombreEOC='Pendiente'
+            )
+            return queryset.annotate(               #annotate agrega un campo adicional a todos los articulos
+                tiene_orden_pendiente=Exists(subquery)
+            ).filter(
+                stockActual__lt=F('puntoPedido'),
+                tiene_orden_pendiente=False
+            )
+        
+class ArticuloAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nombreArticulo', 'stockActual','stockSeguridad','puntoPedido','precioArticulo','costoAlmacenamiento','familiaArticulo')
+    search_fields = ('id','nombreArticulo')
+    ordering = ('id',)
+    list_display_links = ('nombreArticulo',)
+    list_filter = (ArticuloFaltanteFilter,)  # Agregar el filtro por stockActual
+
+class EstadoOrdenCompraAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nombreEOC', 'fechaHoraBajaEOC')
+    ordering = ('id',)
+    list_display_links = ('nombreEOC',)
+    
+class OrdenCompraAdmin(admin.ModelAdmin):
+    list_display = ('id', 'cantidadLote', 'articulo','proveedor')
+    ordering = ('id',)
+
+class ProveedorAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nombreProveedor', 'fechaHoraBajaP','demoraPedido','costoPorPedido')
+    ordering = ('id',)
+    list_display_links = ('nombreProveedor',)
+         
+admin.site.register(FamiliaArticulo,FamiliaArticuloAdmin)
+#@admin.register(FamiliaArticulo)
+admin.site.register(Articulo,ArticuloAdmin)
+admin.site.register(ModeloInventario)
+admin.site.register(EstadoOrdenCompra,EstadoOrdenCompraAdmin)
+admin.site.register(OrdenCompra,OrdenCompraAdmin)
+admin.site.register(Proveedor,ProveedorAdmin)
+
+
+
+
+
+
+
+
+
--- sistemaApp/models.py
+++ sistemaApp/models.py
@@ -1,3 +1,64 @@
 from django.db import models
-
+from django.utils import timezone
+from django.contrib import admin
 # Create your models here.
+
+class ModeloInventario (models.Model):
+    nombreMI = models.CharField(max_length=100,unique=True)
+    fechaHoraBajaMI = models.DateTimeField(default=None,null= True, blank=True) 
+    
+    def __str__(self):
+        return f"{self.nombreMI}"
+    
+class FamiliaArticulo (models.Model):
+    nombreFamiliaArticulo = models.CharField(max_length=100,unique=True)
+    fechaHoraBajaFA = models.DateTimeField(default=None,null= True, blank=True)
+    modeloInventario = models.ForeignKey(ModeloInventario, on_delete=models.PROTECT)
+    
+    def __str__(self):
+        return f"{self.nombreFamiliaArticulo}"
+    
+    def dar_de_baja(self):
+      self.fechaHoraBajaFA = timezone.now()
+      self.save()
+        
+    def dar_de_alta(self):
+        self.fechaHoraBajaFA = None
+        self.save()
+        
+class Articulo (models.Model):
+    nombreArticulo = models.CharField(max_length=100,unique=True)
+    stockActual = models.IntegerField(default=0)
+    stockSeguridad = models.IntegerField(default=0)
+    puntoPedido = models.IntegerField(default=0)
+    precioArticulo = models.IntegerField(default=0)
+    costoAlmacenamiento = models.IntegerField(default=0)
+    familiaArticulo = models.ForeignKey(FamiliaArticulo, on_delete=models.PROTECT,null=True, default=None)
+
+    def __str__(self):
+        return f"{self.nombreArticulo}"
+    
+class EstadoOrdenCompra (models.Model):
+    nombreEOC = models.CharField(max_length=100,unique=True)
+    fechaHoraBajaEOC = models.DateTimeField(default=None,null= True, blank=True) 
+    
+    def __str__(self):
+        return f"{self.nombreEOC}"
+    
+class Proveedor (models.Model):
+    nombreProveedor = models.CharField(max_length=100,unique=True)
+    fechaHoraBajaP = models.DateTimeField(default=None,null= True, blank=True) 
+    demoraPedido = models.IntegerField(default=0,null= True, blank=True)
+    costoPorPedido = models.IntegerField(default=0)
+    
+    def __str__(self):
+        return f"{self.nombreProveedor}"
+     
+class OrdenCompra (models.Model):
+    cantidadLote = models.IntegerField(default=1)
+    estadoOrdenCompra = models.ForeignKey(EstadoOrdenCompra, on_delete=models.PROTECT)
+    articulo = models.ForeignKey(Articulo, on_delete=models.CASCADE, related_name='ordenes_compra')  #related_name se usa para establecer el nombre de la relación inversa desde articulo hacia OrdenCompra
+    proveedor = models.ForeignKey(Proveedor, on_delete=models.PROTECT)
+
+    
+
